85c86d047b3ec46bb7f709ec6c6431ca24da7b93
Alanah <Lana383@yahoo.com>
2013-05-14 11:49:22 -0400

    chp 6 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
   has_secure_password
   
   before_save { |user| user.email = email.downcase }
+  before_save { email.downcase! }
 
   validates(:name, presence: true, length: { maximum: 50 } )
   validates(:password, presence: true, length: { minimum: 6 } )
